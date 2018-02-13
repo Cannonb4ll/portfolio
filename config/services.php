9073fda61b46011fd6299b1ce64cf07857d3eb23
Dennis Smink <dennissmink@gmail.com>
2018-02-13 17:04:33 +0100

    Added mailgun service

--- config/services.php
+++ config/services.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-	'sparkpost' => [
-	    'secret' => '657aea379572f00174db076eb01dd99ae6a214bd',
-	],
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
 ];

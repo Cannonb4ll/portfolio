26b0d02c2ff488bbe2848a97361a60554634a872
Dennis Smink <dennissmink@gmail.com>
2018-04-17 07:50:11 +0200

    Added analytics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,5 +19,13 @@
 </div>
 <script src="{{ $js }}"></script>
 
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-41065522-5"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-41065522-5');
+</script>
 </body>
 </html>
